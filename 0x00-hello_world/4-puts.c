3b162847f65080fa6216976df99362550de4f9af
Ashenafi <ashenafigodana98@gmail.com>
2022-12-08 15:10:34 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,15 @@
  * main - Entry point
  *
  * puts writes or displays what's put into it as an argument
+ *
+ * Return: Always 0 (Success)
  */
+
 #include <stdio.h>
 
 int main()
 {
-	char str[60]="Programming is like building a multilingual puzzle\n";
+	char str[50]="Programming is like building a multilingual puzzle";
 	
 	puts(str);
 	return (0);
