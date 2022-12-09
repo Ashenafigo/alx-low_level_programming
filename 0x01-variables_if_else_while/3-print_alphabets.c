956dade3b06456e8b319e12887cdbeac7984adfc
Ashenafi <ashenafigodana98@gmail.com>
2022-12-09 13:51:11 -0800

    alphaABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print the alphabet both upper and lowercase
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	printf("\n");
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	printf("\n");
+	return (0);
+}
