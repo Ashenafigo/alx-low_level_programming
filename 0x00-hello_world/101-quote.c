d3a89c85460d07906d4d2b33e032c99c0cb9f85c
Ashenafi <ashenafigodana98@gmail.com>
2022-12-10 12:19:23 -0800

    UNIX os

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print a given text
+ *
+ * Return: 1 (standard error)
+ */
+int main(void)
+{
+	char a = 'a', b = 'n', c = 'd',
+	     d = ' ', e = 't', f = 'h',
+	     g = 'a', h = 't', i = ' ',
+	     j = 'p', k = 'i', l = 'e',
+	     m = 'c', n = 'e', o = ' ',
+	     p = 'o', q = 'f', r = ' ', s = 'a', t = 'r', u = 't', v = ' ', w = 'i', x = 's', y = ' ', z = 'u', ab = 's', ac = 'e', ad = 'f', ae = 'u', af = 'l', ag = '\\', ah = '"', jj = ' ', ai = '-', ii = ' ', aj = 'D', ak = 'o', al = 'r', am = 'a', an = ' ', ao = 'K', ap = 'o', aq = 'r', ar = 'p', as = 'a', at = 'r', au = ',', av = ' ', ax = '2', aw = '0', ay = '1', az = '5', ba = '-', bb = '1', bc = '0', bd = '-', be = '1', bf = '9';
+
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	putchar(l);
+	putchar(m);
+	putchar(n);
+	putchar(o);
+	putchar(p);
+	putchar(q);
+	putchar(r);
+	putchar(s);
+	putchar(t);
+	putchar(u);
+	putchar(v);
+	putchar(w);
+	putchar(x);
+	putchar(y);
+	putchar(z);
+	putchar(ab);
+	putchar(ac);
+	putchar(ad);
+	putchar(ae);
+	putchar(af);
+	putchar(ag);
+	putchar(ah);
+	putchar(jj);
+	putchar(ai);
+	putchar(ii);
+	putchar(aj);
+	putchar(ak);
+	putchar(al);
+	putchar(am);
+	putchar(an);
+	putchar(ao);
+	putchar(ap);
+	putchar(aq);
+	putchar(ar);
+	putchar(as);
+	putchar(at);
+	putchar(au);
+	putchar(av);
+	putchar(ax);
+	putchar(aw);
+	putchar(ay);
+	putchar(az);
+	putchar(ba);
+	putchar(bb);
+	putchar(bc);
+	putchar(bd);
+	putchar(be);
+	putchar(bf);
+
+	return (1);
+}
---
+++ 0x00-hello_world/hh.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a='I', b='L', c='Y';
+
+	putchar(a);
+	putchar(b);
+	putchar(c);
+
+	return (0);
+}
